879f5d188dfcb6de20414662cf84f478176b40ab
Ricardo Paes <ricardo@likesistemas.com.br>
2022-05-16 10:15:35 -0300

    💚 Fixing errors `php-cs-fixer`

--- src/Api.php
+++ src/Api.php
@@ -26,7 +26,7 @@ class Api {
 	public function __construct($url, $token) {
 		$this->client = new Client([
 			'base_uri' => $url,
-			'verify' => false
+			'verify' => false,
 		]);
 		$this->token = $token;
 	}
