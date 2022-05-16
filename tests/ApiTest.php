a721e849e47ddf274e2a91d3e410f7654bad763d
Ricardo Araújo Paes <38794747+ricardoapaes@users.noreply.github.com>
2022-05-16 10:17:26 -0300

    🎉 First version (#1)

---
+++ tests/ApiTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace RicardoPaes\Whaticket\Tests;
+
+use PHPUnit\Framework\TestCase;
+use RicardoPaes\Whaticket\Api;
+
+class ApiTest extends TestCase {
+	public function testInstance() {
+		$this->assertTrue(class_exists(Api::class));
+	}
+
+	public function testSendMessage() {
+		$api = new Api(getenv('WHATICKET_BASEURL'), getenv('WHATICKET_TOKEN'));
+		$this->assertTrue($api->sendMessage(getenv('WHATICKET_NUMBER'), 'Whaticket api test in ' . phpversion() . '.', getenv('WHATICKET_ID')));
+	}
+
+	public function testSendMedia() {
+		$api = new Api(getenv('WHATICKET_BASEURL'), getenv('WHATICKET_TOKEN'));
+		$this->assertTrue($api->sendMedia(getenv('WHATICKET_NUMBER'), __DIR__ . '/./logo.png', getenv('WHATICKET_ID')));
+	}
+}
